a166af2b91443c1599a282cec0b88557f5266e87
3dida <44850191+aallali@users.noreply.github.com>
2024-02-25 15:57:10 +0100

    [update] : cast temp type : ft_putstr_fd

--- src/libft/ft_putstr_fd.rs
+++ src/libft/ft_putstr_fd.rs
@@ -3,6 +3,6 @@ use std::{fs::File, io::Write};
 
 pub fn ft_putstr_fd(string: &str, file: &mut File) {
 	
-	let temp = string.as_bytes();
+	let temp: &[u8] = string.as_bytes();
 	file.write_all(temp).expect("Couldn't write to file");
 }
